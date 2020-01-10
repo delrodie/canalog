c7fbb7c23d8f42764cfda68dea1b74e0ea6e3347
Dieudonne AMOIKON <delrodieamoikon@gmail.com>
2020-01-10 12:46:07 +0000

    Gestion des fournisseurs

---
+++ src/Controller/FournisseurController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Fournisseur;
+use App\Form\FournisseurType;
+use App\Repository\FournisseurRepository;
+use Cocur\Slugify\Slugify;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/fournisseur")
+ */
+class FournisseurController extends AbstractController
+{
+    /**
+     * @Route("/", name="fournisseur_index", methods={"GET","POST"})
+     */
+    public function index(Request $request, FournisseurRepository $fournisseurRepository): Response
+    {
+        $fournisseur = new Fournisseur();
+        $form = $this->createForm(FournisseurType::class, $fournisseur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $slugify = new Slugify();
+            $fournisseur->setSlug($slugify->slugify($fournisseur->getNom()));
+            $entityManager->persist($fournisseur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('fournisseur_index');
+        }
+
+        return $this->render('fournisseur/index.html.twig', [
+            'fournisseurs' => $fournisseurRepository->findAll(),
+            'fournisseur' => $fournisseur,
+            'form' => $form->createView(),
+            'current_menu' => 'parametre'
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="fournisseur_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $fournisseur = new Fournisseur();
+        $form = $this->createForm(FournisseurType::class, $fournisseur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($fournisseur);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('fournisseur_index');
+        }
+
+        return $this->render('fournisseur/new.html.twig', [
+            'fournisseur' => $fournisseur,
+            'form' => $form->createView(),
+            'current_menu' => 'parametre'
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}", name="fournisseur_show", methods={"GET"})
+     */
+    public function show(Fournisseur $fournisseur): Response
+    {
+        return $this->render('fournisseur/show.html.twig', [
+            'fournisseur' => $fournisseur,
+            'current_menu' => 'parametre'
+        ]);
+    }
+
+    /**
+     * @Route("/{slug}/edit", name="fournisseur_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Fournisseur $fournisseur, FournisseurRepository $fournisseurRepository): Response
+    {
+        $form = $this->createForm(FournisseurType::class, $fournisseur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('fournisseur_index');
+        }
+
+        return $this->render('fournisseur/edit.html.twig', [
+            'fournisseur' => $fournisseur,
+            'form' => $form->createView(),
+            'fournisseurs' => $fournisseurRepository->findAll(),
+            'current_menu' => 'parametre'
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="fournisseur_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Fournisseur $fournisseur): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$fournisseur->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($fournisseur);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('fournisseur_index');
+    }
+}
---
+++ src/Entity/Fournisseur.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FournisseurRepository")
+ * @UniqueEntity("nom")
+ */
+class Fournisseur
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(?string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
